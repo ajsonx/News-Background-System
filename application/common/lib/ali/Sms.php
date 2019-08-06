2473e7382cf14055d60551316592177a80ca99a5
ajsonx <577429696@qq.com>
2019-08-06 15:00:49 +0800

    add readme

--- application/common/lib/ali/Sms.php
+++ application/common/lib/ali/Sms.php
@@ -95,10 +95,10 @@ class Sms
         $request->setPhoneNumbers($phone);
 
         // 必填，设置签名名称，应严格按"签名名称"填写，请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/sign
-        $request->setSignName("GR新闻");
+        $request->setSignName("");
 
         // 必填，设置模板CODE，应严格按"模板CODE"填写, 请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/template
-        $request->setTemplateCode("SMS_150170001");
+        $request->setTemplateCode("");
 
         // 可选，设置模板参数, 假如模板中存在变量需要替换则为必填项
         $request->setTemplateParam(json_encode(array(  // 短信模板中字段的值
@@ -107,7 +107,7 @@ class Sms
         ), JSON_UNESCAPED_UNICODE));
 
         // 选填，上行短信扩展码（扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段）
-        $request->setSmsUpExtendCode("1234567");
+        $request->setSmsUpExtendCode("");
 
         // 发起访问请求
         $acsResponse = static::getAcsClient()->getAcsResponse($request);
